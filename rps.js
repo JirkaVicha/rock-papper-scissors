3006b3af7b8c31c29e71190ebe34849180c1a1bd
Jiri Vicha <jirivicha@seznam.cz>
2022-09-20 09:19:13 +0200

    Improve function playRound()

--- rps.js
+++ rps.js
@@ -1,34 +1,35 @@
 const choices = ["rock", "paper", "scissors"];
 
-function getComputerChoice() {
-    randomChoices = choices[Math.floor(Math.random() * choices.length)];
-    return randomChoices;
-}
+let playerSelection = prompt("Your choice: ");
+let computerSelection = choices[Math.floor(Math.random() * 3)];
 
+// function getComputerChoice() {
+//     let randomChoices = choices[Math.floor(Math.random() * 3)];
+//     return randomChoices;
+// }
+
+function game() {
+    return playRound(playerSelection, computerSelection); 
+}
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'paper' && computerSelection === 'rock') {
         return "You WIN!";
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         return "You WIN!";
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You WIN!";
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         return "You LOOSE!";
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         return "You LOOSE!";
-    } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
-        return "You WIN!";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "You LOOSE!";
     } else {
-        return "That's DRAW!";
+        return "It's TIE!!";
     }
 }
 
-function game() {
-    return playRound(playerSelection, computerSelection); 
-}
-
-const playerSelection = prompt("Your turn: ");
-const computerSelection = getComputerChoice();
-
 document.getElementById('comp-choice').innerHTML = 
-`The computer's choice: ${getComputerChoice()} and you enter: ${playerSelection}, so the result is: ${game()}`;
+`${game()}, Player: ${playerSelection} Computer: ${computerSelection}`;
 
